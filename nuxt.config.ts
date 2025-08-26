edadaaa3d138bbee676ed4dc1308f6001a1cae4d
Akash Sharma <akashsharma88917@gmail.com>
2025-08-27 03:02:38 +0530

    project setup with Pinia and Tailwind

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss'
+  ],
+  nitro: {
+    storage: {
+      redis: {
+        driver: 'memory'
+      }
+    }
+  }
+})
