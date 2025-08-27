40cb53393a6f2a52d17d76b66a1554e2f76e2865
Akash12-sharma <akashsharma88917@gmail.com>
2025-08-28 01:21:44 +0530

    add some css for button color

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,14 +10,12 @@
          Discover amazing <span class="font-semibold text-purple-600">stories</span>, 
         share your <span class="font-semibold text-pink-600">ideas</span>, and explore the world of creativity.
       </p>
-      
       <NuxtLink 
         to="/posts"
         class="relative inline-block px-8 sm:px-10 py-3 sm:py-4 text-lg font-semibold text-white rounded-2xl
-               bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500
                shadow-xl transition-transform duration-300 hover:scale-105 hover:shadow-2xl
                before:absolute before:inset-0 before:rounded-2xl before:bg-white/20 before:opacity-0 
-               before:transition-opacity before:duration-300 hover:before:opacity-100 overflow-hidden">
+               before:transition-opacity before:duration-300 hover:before:opacity-100 overflow-hidden bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
         View All Posts
       </NuxtLink>  
     </div>
