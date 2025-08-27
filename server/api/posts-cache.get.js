f1b494a12ad00212c2173fa86ca035eae2f85f41
Akash12-sharma <akashsharma88917@gmail.com>
2025-08-27 12:03:17 +0530

    Check if cache is still valid (15 minutes)

--- server/api/posts-cache.get.js
+++ server/api/posts-cache.get.js
@@ -1,4 +1,17 @@
 export default defineEventHandler(async (event) => {
   const storage = useStorage('redis')
-  const cachedPosts = await storage.getItem('posts')
+  const cachedPosts = await storage.getItem('posts');
+  if (cachedPosts) {
+    const cacheTime = await storage.getItem('posts_timestamp')
+    const now = Date.now()
+    
+    if (now - cacheTime < 900000) {
+      return cachedPosts
+    } else {
+      await storage.removeItem('posts')
+      await storage.removeItem('posts_timestamp')
+    }
+  }
+  
+  return null
 })
